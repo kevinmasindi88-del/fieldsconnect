4a9f120589ced2956e2c34dfc7f9dc85932c2074
Kevin Masindi <234076036+kevinmasindi88-del@users.noreply.github.com>
2026-07-04 19:32:42 +0200

    feat: add timeline posts baseline

---
+++ app/timeline/page.tsx
@@ -0,0 +1,9 @@
+﻿import { TimelineWorkflow } from "@/components/timeline/TimelineWorkflow";
+
+export default function TimelinePage() {
+  return (
+    <main className="min-h-screen">
+      <TimelineWorkflow />
+    </main>
+  );
+}
