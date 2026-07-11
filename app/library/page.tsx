e637850f79acfb390a19982bcef7e34e496ddd07
kevinmasindi88-del <kevinmasindi88@gmail.com>
2026-07-11 13:55:56 +0200

    feat: wire expandable library results list

--- app/library/page.tsx
+++ app/library/page.tsx
@@ -1,10 +1,12 @@
 import { LibraryDiscoveryFilters } from "@/components/library/LibraryDiscoveryFilters";
+import { LibraryResultsEnhancer } from "@/components/library/LibraryResultsEnhancer";
 import { LibraryWorkflow } from "@/components/library/LibraryWorkflow";
 
 export default function LibraryPage() {
   return (
     <main className="min-h-screen">
       <LibraryDiscoveryFilters />
+      <LibraryResultsEnhancer />
       <LibraryWorkflow />
     </main>
   );
