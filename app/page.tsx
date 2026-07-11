ea926a037ddbe5854b27cdea8de64499017c8090
kevinmasindi88-del <kevinmasindi88@gmail.com>
2026-07-11 12:45:45 +0200

    Enable deleting own timeline comments

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { TimelineWorkflow } from "@/components/timeline/TimelineWorkflow";
 import { TimelineCommentsEnhancer } from "@/components/timeline/TimelineCommentsEnhancer";
 import { TimelinePostDeleteEnhancer } from "@/components/timeline/TimelinePostDeleteEnhancer";
+import { TimelineCommentDeleteEnhancer } from "@/components/timeline/TimelineCommentDeleteEnhancer";
 
 export default function HomePage() {
   return (
     <main className="min-h-screen">
       <TimelineCommentsEnhancer />
       <TimelinePostDeleteEnhancer />
+      <TimelineCommentDeleteEnhancer />
       <TimelineWorkflow />
     </main>
   );
