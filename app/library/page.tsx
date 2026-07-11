fa2cb56c133bf5fdfe28e843818407fef4d23814
kevinmasindi88-del <kevinmasindi88@gmail.com>
2026-07-11 16:57:56 +0200

    refactor: consolidate library discovery into public library

--- app/library/page.tsx
+++ app/library/page.tsx
@@ -1,10 +1,8 @@
-import { LibraryDiscoveryFilters } from "@/components/library/LibraryDiscoveryFilters";
 import { LibraryWorkflow } from "@/components/library/LibraryWorkflow";
 
 export default function LibraryPage() {
   return (
     <main className="min-h-screen">
-      <LibraryDiscoveryFilters />
       <LibraryWorkflow />
     </main>
   );
