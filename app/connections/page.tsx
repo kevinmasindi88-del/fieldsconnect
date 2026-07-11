8af0aa8fd873d9e9a1a7a56414687a67f4cc6806
kevinmasindi88-del <kevinmasindi88@gmail.com>
2026-07-11 10:41:56 +0200

    Rename accepted connections and show count

--- app/connections/page.tsx
+++ app/connections/page.tsx
@@ -1,8 +1,10 @@
 import { ConnectionWorkflow } from "@/components/connections/ConnectionWorkflow";
+import { ConnectionsHeadingEnhancer } from "@/components/connections/ConnectionsHeadingEnhancer";
 
 export default function ConnectionsPage() {
   return (
     <main className="min-h-screen">
+      <ConnectionsHeadingEnhancer />
       <ConnectionWorkflow />
     </main>
   );
