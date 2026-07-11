cbec1aafc2e78dab40a651991b28371570f62901
kevinmasindi88-del <kevinmasindi88@gmail.com>
2026-07-11 11:07:48 +0200

    Enable collapsible timeline comments

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import { TimelineWorkflow } from "@/components/timeline/TimelineWorkflow";
+import { TimelineCommentsEnhancer } from "@/components/timeline/TimelineCommentsEnhancer";
 
 export default function HomePage() {
   return (
     <main className="min-h-screen">
+      <TimelineCommentsEnhancer />
       <TimelineWorkflow />
     </main>
   );
