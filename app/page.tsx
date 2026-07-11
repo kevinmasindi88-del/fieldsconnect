48644c2d8119b5998d63d8d9c8a0b9639d7753b5
kevinmasindi88-del <kevinmasindi88@gmail.com>
2026-07-11 11:34:52 +0200

    Enable deletion of users' own timeline posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { TimelineWorkflow } from "@/components/timeline/TimelineWorkflow";
 import { TimelineCommentsEnhancer } from "@/components/timeline/TimelineCommentsEnhancer";
+import { TimelinePostDeleteEnhancer } from "@/components/timeline/TimelinePostDeleteEnhancer";
 
 export default function HomePage() {
   return (
     <main className="min-h-screen">
       <TimelineCommentsEnhancer />
+      <TimelinePostDeleteEnhancer />
       <TimelineWorkflow />
     </main>
   );
