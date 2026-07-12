0d0b0c76d89931f248d867952eb482c5069d5d6e
kevinmasindi88-del <kevinmasindi88@gmail.com>
2026-07-12 16:38:13 +0200

    feat: add moderation nominations page

---
+++ app/moderation/nominations/page.tsx
@@ -0,0 +1,9 @@
+import { RoleNominationWorkflow } from "@/components/moderation/RoleNominationWorkflow";
+
+export default function ModerationNominationsPage() {
+  return (
+    <main className="min-h-screen bg-gray-50">
+      <RoleNominationWorkflow />
+    </main>
+  );
+}
